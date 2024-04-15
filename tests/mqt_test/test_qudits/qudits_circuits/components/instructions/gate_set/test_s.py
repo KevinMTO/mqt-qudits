a57d189e0fe8bbdbc63510cc5fbf4b6f6ed84cc0
peyman <peyman.rad@gmx.de>
2024-04-15 15:22:11 +0200

    basic tests for S gate

--- tests/mqt_test/test_qudits/qudits_circuits/components/instructions/gate_set/test_s.py
+++ tests/mqt_test/test_qudits/qudits_circuits/components/instructions/gate_set/test_s.py
@@ -3,20 +3,22 @@ from unittest import TestCase
 import numpy as np
 from mqt.qudits.qudit_circuits.circuit import QuantumCircuit
 
-
-def omega_d(d):
-    return np.e ** (2 * np.pi * 1j / d)
+from tests.mqt_test.test_qudits.qudits_circuits.components.instructions.gate_set.utils import omega_d
 
 
-class TestZ(TestCase):
+class TestS(TestCase):
     def setUp(self):
-        self.circuit_23 = QuantumCircuit(1, [2], 0)
+        self.circuit_23 = QuantumCircuit(1, [2, 3], 0)
 
     def test___array__(self):
         s_0 = self.circuit_23.s(0)
         matrix_0 = s_0.to_matrix(identities=0)
         assert np.allclose(np.array([[1, 0], [0, 1j]]), matrix_0)
 
+        s_1 = self.circuit_23.s(1)
+        matrix_1 = s_1.to_matrix(identities=0)
+        assert np.allclose(np.array([[1, 0, 0], [0, omega_d(3), 0], [0, 0, 1]]), matrix_1)
+
     def test_validate_parameter(self):
-        z = self.circuit_23.z(0)
-        assert z.validate_parameter()
+        s = self.circuit_23.s(0)
+        assert s.validate_parameter()
